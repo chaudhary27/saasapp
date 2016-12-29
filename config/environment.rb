f67ee723b594a81e671d7b4b5ed67980de6ceb37
faisal farooq <chaudhary.fsl@gmail.com>
2016-12-28 23:42:18 -0500

    error fix

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :port => ENV['MAILGUN_SMTP_PORT'],
   :address => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name => ENV['MAILGUN_SMTP_LOGIN'].
+  :user_name => ENV['MAILGUN_SMTP_LOGIN'],
   :password => ENV['MAILGUN_SMTP_PASSWORD'],
   :domain => 'hidden-lake-34149.herokuapp.com',
   :authentication => :plain
