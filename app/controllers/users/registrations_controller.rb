e1e40f8bfe244c76939a0ec342c130414376587c
faisal farooq <chaudhary.fsl@gmail.com>
2017-01-05 07:06:22 -0500

    ui fixes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+
+  before_action :select_plan, only: :new
   # Extend default Devise gem behavior so that user signing up with pro
   # account with plan_id 2 save with special subs function
   # Otherwise devise saves as normal
@@ -14,4 +16,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
+
+  private
+    def select_plan
+      unless (params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = "Please select a membership plan to sign up."
+        redirect_to root_path
+      end
+    end
 end
