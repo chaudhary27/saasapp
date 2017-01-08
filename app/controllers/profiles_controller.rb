e3a3ad0ae2c66a953832e7b502fa5190ace7c772
faisal farooq <chaudhary.fsl@gmail.com>
2017-01-08 07:17:22 -0500

    User show page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,13 +3,23 @@ class ProfilesController < ApplicationController
     @profile = Profile.new
   end
 
+  # POST TO /users/:user_id/profile
   def create
-    @profile = Profile.new(profile_params)
+    # Ensure that we have user who is filling the form
+    @user = User.find(params[:user_id])
+    # Create profile linked to this user
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile Updated"
+      redirect_to root_path
+    else
+      render action: :new
+    end
   end
 
 
   private
     def profile_params
-      params.
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
 end
